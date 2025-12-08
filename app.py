972a08a548f50f2c89c74c4a73bf449966cf4cab
Muad-syntax <mohammad.suaebummuad9@smk.belajar.id>
2025-12-08 07:50:52 +0700

    menambah data base

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data_pengguna.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+
+class Pengunjung(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nama = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Pengunjung {self.nama}>'
+    
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def halaman_utama():
     if request.method == "POST":
+
         nama_dari_from = request.form["nama_input"]
-        return render_template("index.html", orang=nama_dari_from)
+
+        data_baru = Pengunjung(nama=nama_dari_from)
+
+        db.session.add(data_baru)
+        db.session.commit()
+
+        list_pengunjung =Pengunjung.query.all()
+        return render_template("index.html", daftar=list_pengunjung)
 
     return render_template("index.html")
 
